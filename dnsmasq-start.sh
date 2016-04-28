f6c88932576dc4539c7160f20b3bce3e1469d700
Marc <mgbuddy@gmail.com>
2016-04-28 10:34:56 +0000

    Added dnsmasq

---
+++ dnsmasq-start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+BASE=`dirname $0`
+pushd $BASE
+NIC="eth0"
+name="dnsmasq"
+export MY_IP=$(ifconfig $NIC | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
+
+[ ! -d dnsmasq.hosts ] && mkdir dnsmasq.hosts
+[ -e dnsmasq.hosts/calavera ] && rm dnsmasq.hosts/calavera
+
+# Start dnsmasq server
+docker stop dnsmasq
+docker rm dnsmasq
+docker run -v="$(pwd)/dnsmasq.hosts:/dnsmasq.hosts" --name=${name} -p=${MY_IP}':53:5353/udp' -d sroegner/dnsmasq > /tmp/out 2>&1
+echo "-- Dnsmasq created"
