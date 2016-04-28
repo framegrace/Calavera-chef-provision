ec32a710617eec1140752bd10c58114956f104b3
Marc <mgbuddy@gmail.com>
2016-04-28 01:26:28 +0000

    Added stress test

---
+++ scripts-shop/stress-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "### Running JMeter performance test ###"
+
+# Clear out old results
+rm $WORKSPACE/jmeter.jtl
+
+# Run the tests
+echo "## Running the tests"
+cd "$WORKSPACE/jmeter"
+
+jmeter -n -t jmeter.jmx -l $WORKSPACE/jmeter.jtl
