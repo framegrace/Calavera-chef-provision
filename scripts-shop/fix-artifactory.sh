ad8d267649bdfdfbc1afd493d155dcfbdbbba563
Marc <mgbuddy@gmail.com>
2016-04-28 10:33:50 +0000

    Do not reduce that much

--- scripts-shop/fix-artifactory.sh
+++ scripts-shop/fix-artifactory.sh
@@ -1,2 +1,2 @@
-ssh espina "sed 's/Xmx2g/Xmx512m/g' /opt/artifactory-3.5.1/etc/default > /tmp/art;mv /tmp/art /opt/artifactory-3.5.1/etc/default"
+ssh espina "sed 's/Xmx2g/Xmx1g/g' /opt/artifactory-3.5.1/etc/default > /tmp/art;mv /tmp/art /opt/artifactory-3.5.1/etc/default"
 ssh espina "/etc/init.d/artifactory restart"
--- scripts-shop/fix-jenkins-git.sh
+++ scripts-shop/fix-jenkins-git.sh
@@ -1,5 +1,5 @@
 ssh hombros "apt-get install -y git"
-ssh hombros "sed 's/#JAVA_ARGS=\"-Xmx256m\"/JAVA_ARGS=\"-Xmx256m\"/g' /etc/default/jenkins > /tmp/jenkins;mv /tmp/jenkins /etc/default/jenkins"
+ssh hombros "sed 's/#JAVA_ARGS=\"-Xmx256m\"/JAVA_ARGS=\"-Xmx1g\"/g' /etc/default/jenkins > /tmp/jenkins;mv /tmp/jenkins /etc/default/jenkins"
 ssh hombros "/etc/init.d/jenkins restart"
 scp /opt/Calavera-chef-provision/scripts-shop/deploy.sh cara:/usr/bin
 scp /opt/Calavera-chef-provision/scripts-shop/send-statsd-notification.sh brazos:/usr/local/bin/
