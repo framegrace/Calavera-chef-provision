8723b178338f45955715e4bc2d9088329bf3de93
Marc <mgbuddy@gmail.com>
2016-04-28 01:02:48 +0000

    Reduce jenkins memory

--- scripts-shop/fix-jenkins-git.sh
+++ scripts-shop/fix-jenkins-git.sh
@@ -1,4 +1,6 @@
 ssh hombros "apt-get install -y git"
+ssh hombros "sed 's/#JAVA_ARGS=\"-Xmx256m\"/JAVA_ARGS=\"-Xmx256m\"/g' /etc/default/jenkins > /tmp/jenkins;mv /tmp/jenkins /etc/default/jenkins"
+ssh hombros "/etc/init.d/jenkins restart"
 scp /opt/Calavera-chef-provision/scripts-shop/deploy.sh cara:/usr/bin
 scp /opt/Calavera-chef-provision/scripts-shop/send-statsd-notification.sh brazos:/usr/local/bin/
 scp /opt/Calavera-chef-provision/scripts-shop/download_artifactory.sh brazos:/usr/local/bin/
